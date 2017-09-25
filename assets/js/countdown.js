659de5b2a727147ca5c449b01ef6325b27faac0d
Lucas Craveiro Paes <lucascraveiropaes123@gmail.com>
2017-09-25 16:50:55 -0300

    Corrigindo contador

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 25, 2017 18:00:00").getTime();
+var countDownDate = new Date("Oct 25, 2017 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,18 +17,10 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Test the results in case it's 0. If it's, then don't show
-    if(days != 0) {
-        document.getElementById("days").innerHTML = days+"d";
-    }
-    if(hours != 0) {
-        document.getElementById("hours").innerHTML = hours+"h";
-    }
-    if(minutes != 0) {
-        document.getElementById("minutes").innerHTML = minutes+"m";
-    }
-    if(seconds != 0) {
-        document.getElementById("seconds").innerHTML = seconds+"s";
-    }
+    document.getElementById("days").innerHTML = days+"d";
+    document.getElementById("hours").innerHTML = hours+"h";
+    document.getElementById("minutes").innerHTML = minutes+"m";
+    document.getElementById("seconds").innerHTML = seconds+"s";
     
     // If the count down is over, write some text 
     if (distance < 0) {
